5f4bb2d2e3aa8dbfc7ff3143751340d88ae860b5
celso <celso.san2101@alumnos.ubiobio.cl>
2025-06-01 20:08:15 -0400

    Update testRoutes.js

--- src/routes/testRoutes.js
+++ src/routes/testRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Ruta de prueba "Hola Mundo"
 router.get('/hola', (req, res) => {
-  res.json({ mensaje: 'Hola ' });
+  res.json({ mensaje: 'Hola amigo' });
 });
 
 
