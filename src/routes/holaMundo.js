0791c2d566931e6021600526fa86b7930e1816f3
celso <celso.san2101@alumnos.ubiobio.cl>
2025-06-01 20:02:23 -0400

    Update holaMundo.js

--- src/routes/holaMundo.js
+++ src/routes/holaMundo.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Ruta de prueba "Hola Mundo"
 router.get('/hola', (req, res) => {
-  res.json({ mensaje: 'Hola Mundito' });
+  res.json({ mensaje: 'Hola ricardios' });
 });
 
 module.exports = router;
