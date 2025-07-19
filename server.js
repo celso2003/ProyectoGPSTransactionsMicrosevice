de4078e40d4c72c5f7f5e988b0e5a5446bec4fb4
celso <celso.san2101@alumnos.ubiobio.cl>
2025-07-19 17:40:16 -0400

    eliminas

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const fs = require('fs');
 // Import custom modules
 const { sequelize, connectDB } = require('./src/config/database');
 const logger = require('./src/utils/logger');
-const { router: metricsRouter, metrics } = require('./src/routes/metricsRoutes');
 
 // Create Express app
 const app = express();
@@ -32,35 +31,6 @@ app.use(morgan('common', {
   stream: { write: message => logger.info(message.trim()) }
 }));
 
-// Configurar middleware de métricas
-app.use((req, res, next) => {
-  const start = Date.now();
-  
-  // Cuando la respuesta finaliza, registrar métricas
-  res.on('finish', () => {
-    const duration = Date.now() - start;
-    
-    // Registrar conteo de solicitudes
-    metrics.httpRequestCounter.inc({
-      method: req.method,
-      route: req.route ? req.route.path : req.path,
-      status_code: res.statusCode
-    });
-    
-    // Registrar duración de la solicitud
-    metrics.httpRequestDurationMicroseconds.observe(
-      {
-        method: req.method,
-        route: req.route ? req.route.path : req.path,
-        status_code: res.statusCode
-      },
-      duration
-    );
-  });
-  
-  next();
-});
-
 // Importar rutas
 const transactionRoutes = require('./src/routes/transactionRoutes'); // Nueva ruta
 const healthRoutes = require('./src/routes/healthRoutes');
@@ -68,7 +38,6 @@ const healthRoutes = require('./src/routes/healthRoutes');
 // Definir manejadores de rutas
 app.use('/transactions', transactionRoutes); // Nuevo manejador de ruta
 app.use('/health', healthRoutes);
-app.use('/metrics', metricsRouter);
 
 
 // Manejar errores 404
